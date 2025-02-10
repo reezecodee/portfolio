07079a370ef0cd14f9942fc01e2d40fd998b82bb
Reeze <azfaalharits25@gmail.com>
2025-02-10 09:57:21 +0700

    remove vue js

--- src/components/SkillSets.vue
+++ src/components/SkillSets.vue
@@ -41,11 +41,6 @@
         </button>
         <button
             class="p-3 bg-slate-800/70 hover:bg-slate-800/100 duration-200 backdrop-blur-sm rounded-lg flex items-center gap-2">
-            <svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-vue"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M16.5 4l-4.5 8l-4.5 -8" /><path d="M3 4l9 16l9 -16" /></svg>
-            VueJS
-        </button>
-        <button
-            class="p-3 bg-slate-800/70 hover:bg-slate-800/100 duration-200 backdrop-blur-sm rounded-lg flex items-center gap-2">
             <svg  xmlns="http://www.w3.org/2000/svg"  width="18"  height="18"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-brand-flutter"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 14l-3 -3l8 -8h6z" /><path d="M14 21l-5 -5l5 -5h5l-5 5l5 5z" /></svg>
             Flutter
         </button>
