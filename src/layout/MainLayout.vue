113b3d146e8040e88b40302064072911d4203b32
Reeze <azfaalharits25@gmail.com>
2025-04-02 21:49:52 +0700

    refactor: margin top in layout

--- src/layout/MainLayout.vue
+++ src/layout/MainLayout.vue
@@ -1,6 +1,6 @@
 <template>
     <Navbar></Navbar>
-    <div class="mt-36 plus-jakarta-sans-text">
+    <div class="mt-28 plus-jakarta-sans-text">
         <div class="container mx-auto px-4 md:px-8 text-justify md:text-start">
             <div class="flex justify-center">
                 <div class="max-w-3xl text-white text-md">
